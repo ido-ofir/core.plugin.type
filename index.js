adf2abc8fce7789592269864650404662b69e9f9
ido <ido@dooble.co.il>
2018-01-07 01:01:42 +0200

    v 1.0.0

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 module.exports = {
     name: 'core.plugin.type',
-    dependencies: [
-        'core.plugin.get-definition-object'
-    ],
+    dependencies: [],
     init(definition, done){
 
         var core = this;
@@ -38,6 +36,48 @@ module.exports = {
                 var definition = this.getDefinitionObject(name, dependencies, get, 'type');    
                 return this.build(definition);
             },
+            build(source, done) {
+
+                var core = this;
+    
+                if (!source) { return source; }
+    
+                var typeName = source['$_type'];
+    
+                if (!typeName) {
+                    if(core.isArray(source)){
+                        return source.map(t => core.build(t));
+                    }
+                    if(core.isObject(source)){
+                        return core.assign({}, source, function(property, key, source){
+                            return core.build(property);
+                        });
+                    }
+                    return source;
+                }
+    
+                var parent = null;
+                var result = source;
+                var type = core.types[typeName];
+    
+                if (!type) throw new Error(`cannot find type '${typeName}'`);
+                if(!type.build){ return type; }
+    
+                if(type.extends){
+                    parent = core.types[type.extends];
+                    if(!parent){
+                        throw new Error(`cannot find type '${type.extends}', '${ typeName }' tries to extend it.`);
+                    }
+                }
+    
+                return type.build.call(core, result, function(built, done){
+                    if(!parent){ 
+                        return built; 
+                    }
+                    return core.build(core.assign({}, built, { $_type: parent.name }), done);
+                }, done);
+    
+            }
         };
 
         core.extend(extend);
